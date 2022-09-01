4891330d0f2f7a0f76f6f364b2d3b6f6c06ba7f9
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-09-01 13:38:39 -0500

    CTA Section Commit

---
+++ components/CtaSection/CtaSection.js
@@ -0,0 +1,11 @@
+import styles from './CtaSection.module.css'
+
+export default function CtaSection(props) {
+    return (
+        <section>
+            <div className="container">
+                <p>Hey Ian </p>
+            </div>
+        </section>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,9 @@ import type { AppProps } from 'next/app'
 import { builder } from '@builder.io/react'
 import builderConfig from '../config/builder'
 import { ContextMenu } from '@builder.io/personalization-context-menu'
+import "../components/CtaSection/CtaSection.builder"
 import '../assets/index.css'
-// only needed for context menu styling
-import '@szhsin/react-menu/dist/index.css'
-import '@szhsin/react-menu/dist/transitions/slide.css'
-import '@builder.io/widgets/dist/lib/builder-widgets-async'
+
 
 builder.init(builderConfig.apiKey)
 
